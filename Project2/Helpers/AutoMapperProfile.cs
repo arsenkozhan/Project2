396f60ee64351cefd1f9578bb6f52aa80823e9d5
Артур Аксиневич <artaksin@gmail.com>
2018-11-13 11:07:16 +0200

    Add project files.

---
+++ Project2/Helpers/AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Project2.Dtos;
+using Project2.Entities;
+
+namespace Project2.Helpers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, UserDto>();
+            CreateMap<UserDto, User>();
+        }
+    }
+}
---
+++ Project2/Helpers/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project2.Entities;
+
+namespace Project2.Helpers
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
